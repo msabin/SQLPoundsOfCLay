bd5329c42e29e539a19d5ec6e6b391f7acd5d69d
Henderson Reed Hummel <reed.hummel@gmail.com>
2023-09-20 16:09:08 -0700

    began tying in sqlite3.

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,58 @@
 const http = require("http");
 const PORT = process.env.PORT || 5000;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data.sqlite');
+db.run("CREATE TABLE IF NOT EXISTS artists (artistId INT, name TEXT)");
+db.close();
 
 const server = http.createServer(async (req, res) => {
     //set the request route
-    if (req.url === "/" && req.method === "GET") {
-        //response headers
-        res.writeHead(200, { "Content-Type": "application/json" });
-        //set the response
-        res.write("Recurse is super cool!");
-        //end the response
-        res.end();
+    if(req.method === "GET") {
+        getEndpoints(req,res);
     }
-
-    // If no route present
+    else if(req.method === "POST") {
+        postEndpoints(req,res);
+    }
     else {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Route not found" }));
+        res.writeHead(405, { "Content-Type": "text/plain" });
+        res.end("Only POST and GET requests are supported at this time.");
     }
 });
+
+function getEndpoints(req,res) {
+    switch(req.url) {
+        case '/':
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write("Recurse is super cool!");
+            res.end();
+            break;
+        default:
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Route not found");
+            break;
+    }
+}
+
+function postEndpoints(req,res) {
+    switch(req.url) {
+        case '/create/artist':
+            const db = new sqlite3.Database('data.sqlite');
+            db.run("INSERT INTO artists (artistId, name) VALUES (1, 'Pink Floyd')");
+            db.close();
+            console.log(req);
+
+            res.writeHead(201, { "Content-Type": "text/plain" });
+            res.write("saved pink floyd\n");
+            res.end();
+            break;
+        default:
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Route not found");
+            break;
+    }
+}
 
 server.listen(PORT, () => {
     console.log(`server started on port: ${PORT}`);
 });
+
